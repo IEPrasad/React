29cb7c02659d6fc7af21ae97369e062394e13514
IEPrasad <ieprasad23@gmail.com>
2025-09-11 22:40:19 +0530

    Alert with close button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,14 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="info" onClick={() => console.log("Clicked")}>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color="info" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
